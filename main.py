4b6451c97f7b1c97db87bc1746e35756304e5937
zzzturka <zeturka@gmail.com>
2021-12-24 18:20:00 +1300

    test

--- main.py
+++ main.py
@@ -20,46 +20,47 @@ while game_on:
 
     if len(player_one.all_cards) == 0:
         print("Player one is out of cards, player two won!")
-        print(f"Currently on round {round}")
+        print(f"Currently on round1 {round}")
         game_on = False
         break
     elif len(player_two.all_cards) == 0:
         print("Player two is out of cards, player one won!")
-        print(f"Currently on round {round}")
+        print(f"Currently on round2 {round}")
         game_on = False
         break
 
     # player's cards on the table:
 
     player_one_cards = [player_one.remove_one()]
-
     player_two_cards = [player_two.remove_one()]
 
     # while at_war:
+
     at_war = True
+
     while at_war:
 
         if player_one_cards[-1].value > player_two_cards[-1].value:
             player_one.add_cards(player_one_cards)
             player_one.add_cards(player_two_cards)
-            print(f"Currently on round {round}")
+            print(f"Currently on round3 {round}")
             at_war = False
         elif player_two_cards[-1].value > player_one_cards[-1].value:
             player_two.add_cards(player_one_cards)
             player_two.add_cards(player_two_cards)
-            print(f"Currently on round {round}")
+            print(f"Currently on round4 {round}")
             at_war = False
 
         else:
             print("WAR!")
             if len(player_one.all_cards) < 3:
                 print("player one is unable to declare war!\nPlayer two wins!")
-                print(f"Currently on round {round}")
+                print(f"Currently on round5 {round}")
                 game_on = False
                 break
             elif len(player_two.all_cards) < 3:
                 print("player two is unable to declare war!\nPlayer one wins!")
-                print(f"Currently on round {round}")
+                print(f"Currently on round6 {round}")
                 game_on = False
                 break
             else:
